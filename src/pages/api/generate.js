86f0abb6c976174059cc054f680180c23317a276
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-12-22 17:00:53 -0300

    test change

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createImage({
     prompt: req.body.text,
-    n: 4,
+    n: 6,
     size: "1024x1024",
   });
   res.status(200).json({result: completion.data.data})
