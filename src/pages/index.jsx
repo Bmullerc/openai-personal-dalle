6e9af1c2b83d377d14010b3e73dc66222675cd1a
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-12-23 20:13:21 -0300

    try: trying to make a save button, no joy so far

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false)
   const [imageInput, setImageInput] = useState("");
   const [result, setResult] = useState();
   const [history, setHistory] = useState([]);
+
+  const imageRef = useRef()
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -44,9 +46,9 @@ export default function Home() {
     } else return
   }
 
-  function handleHistoryClick() {
-    setImageInput()
-  }
+  // function handleHistoryClick() {
+  //   setImageInput()
+  // }
 
   return (
     <>
@@ -54,8 +56,8 @@ export default function Home() {
         <title>OpenAI - Create Image</title>
       </Head>
 
-      <main className="h-screen bg-zinc-200">
-        <div className="flex flex-col gap-10 py-10">
+      <main className="h-screen bg-zinc-200 pb-20">
+        <div className="flex flex-col gap-10 pb-10">
           <h3 className="text-center font-bold text-5xl tracking-wide">Create an Image, human.<Image src="/robot1.png" height={150} width={150} /></h3>
           <form onSubmit={onSubmit} className="flex flex-row gap-5 items-center justify-center text-center">
             <input
@@ -82,18 +84,23 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col py-4 px-4 text-lg bg-zinc-300 gap-4">
-              {history.map(text => <p onClick={handleHistoryClick}>{text}</p>)}
+              {history.map(text => <p>{text}</p>)}
             </div>
           </section>}
 
         </div>
 
-        <div className="grid grid-flow-col m-auto gap-8 items-center justify-center px-10">
+        <div className="grid grid-flow-col m-auto gap-8 items-center justify-center px-10 bg-zinc-200">
           {isLoading ? <h3>Loading...</h3> : result && result.map((object) => {
             return (
-              <>
-                <Image src={object.url} key={object.index} height={400} width={400} className="rounded-sm" />
-              </>
+              <div className="flex flex-col gap-3">
+                <Image ref={imageRef} src={object.url} key={object.index} height={400} width={400} className="rounded-sm" />
+                {/* <button
+                  onClick={handleSave}
+                  className="bg-zinc-700 text-white w-fit m-auto font-bold text-base hover:opacity-70 hover:scale-95 duration-200 rounded-lg tracking-tighter uppercase py-2 px-3"
+                >Save Me
+                </button> */}
+              </div>
             )
           })}
 
